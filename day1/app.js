b4925fb3545b859d82c32c0eebf708e4fb18e440
priyakesarwani0101 <priyakesarwani0101@gmail.com>
2023-01-04 17:51:15 +0530

    breaking the coding by creating multiple files...

---
+++ day1/app.js
@@ -0,0 +1,17 @@
+    //lets create an element with the help of react library:
+
+    
+    const heading = React.createElement("h1",{
+        id:"heading",
+    },"Heading1");
+
+
+    const heading2 = React.createElement("h2",{},"heading2");
+
+    const container = React.createElement("div",{
+        id:"container"
+    },[heading,heading2]);
+
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(container);
